c09215b729bee737e32ae3088a660077a8507901
Yadnyesh Bharat Juvekar <yadnyesh.juvekar@gmail.com>
2018-06-23 17:33:48 +0530

    First commit

---
+++ src/main/java/io/yadnyesh/security/springbootjwtsecurity/SpringbootJwtSecurityApplication.java
@@ -0,0 +1,12 @@
+package io.yadnyesh.security.springbootjwtsecurity;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringbootJwtSecurityApplication {
+	
+	public static void main(String[] args) {
+		SpringApplication.run(SpringbootJwtSecurityApplication.class, args);
+	}
+}
