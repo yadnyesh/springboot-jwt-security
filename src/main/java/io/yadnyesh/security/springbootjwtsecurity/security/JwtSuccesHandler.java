58dce9fd53295697aa0dd2da78171adc775cf4b0
Yadnyesh Bharat Juvekar <yadnyesh.juvekar@gmail.com>
2018-06-24 10:08:17 +0530

    Udpates to override on Authentication Success.

--- src/main/java/io/yadnyesh/security/springbootjwtsecurity/security/JwtSuccesHandler.java
+++ src/main/java/io/yadnyesh/security/springbootjwtsecurity/security/JwtSuccesHandler.java
@@ -1,6 +1,20 @@
 package io.yadnyesh.security.springbootjwtsecurity.security;
 
+import org.springframework.security.core.Authentication;
+import org.springframework.security.web.authentication.AuthenticationSuccessHandler;
 
-public class JwtSuccesHandler {
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
 
+public class JwtSuccesHandler implements AuthenticationSuccessHandler {
+	
+	public JwtSuccesHandler() {
+	}
+	
+	@Override
+	public void onAuthenticationSuccess(HttpServletRequest httpServletRequest, HttpServletResponse httpServletResponse, Authentication authentication) throws IOException, ServletException {
+		System.out.println("User Authenticated Successfully");
+	}
 }
