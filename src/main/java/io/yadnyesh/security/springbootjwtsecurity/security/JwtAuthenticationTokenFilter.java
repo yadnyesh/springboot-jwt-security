c42f0a066e2aff3a7945e726582ee505f8e8802f
Yadnyesh Bharat Juvekar <yadnyesh.juvekar@gmail.com>
2018-06-24 10:06:21 +0530

    Override success Autho method

--- src/main/java/io/yadnyesh/security/springbootjwtsecurity/security/JwtAuthenticationTokenFilter.java
+++ src/main/java/io/yadnyesh/security/springbootjwtsecurity/security/JwtAuthenticationTokenFilter.java
@@ -1,9 +1,11 @@
 package io.yadnyesh.security.springbootjwtsecurity.security;
 
+import io.yadnyesh.security.springbootjwtsecurity.model.JwtAuthenticationToken;
 import org.springframework.security.core.Authentication;
 import org.springframework.security.core.AuthenticationException;
 import org.springframework.security.web.authentication.AbstractAuthenticationProcessingFilter;
 
+import javax.servlet.FilterChain;
 import javax.servlet.ServletException;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
@@ -11,12 +13,29 @@ import java.io.IOException;
 
 public class JwtAuthenticationTokenFilter extends AbstractAuthenticationProcessingFilter {
 	
-	protected JwtAuthenticationTokenFilter() {
+	public JwtAuthenticationTokenFilter() {
 		super("/**");
 	}
 	
 	@Override
 	public Authentication attemptAuthentication(HttpServletRequest httpServletRequest, HttpServletResponse httpServletResponse) throws AuthenticationException, IOException, ServletException {
-		return null;
+		
+		String header = httpServletRequest.getHeader("Authorization");
+		
+		if(header == null || !header.startsWith("Token ")){
+			throw new RuntimeException("JWT Token is missing");
+		}
+		
+		String authenticationToken = header.substring(6);
+		
+		JwtAuthenticationToken token = new JwtAuthenticationToken(authenticationToken);
+		
+		return getAuthenticationManager().authenticate(token);
+	}
+	
+	@Override
+	protected void successfulAuthentication(HttpServletRequest request, HttpServletResponse response, FilterChain chain, Authentication authResult) throws IOException, ServletException {
+		super.successfulAuthentication(request, response, chain, authResult);
+		chain.doFilter(request, response);
 	}
 }
