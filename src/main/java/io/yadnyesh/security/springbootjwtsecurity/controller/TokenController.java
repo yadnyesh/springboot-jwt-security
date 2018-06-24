01c12f610a8c801fb6426a041e44c9a7aed8bb10
Yadnyesh Bharat Juvekar <yadnyesh.juvekar@gmail.com>
2018-06-24 16:24:14 +0530

    TokenController - User to POST to fetch a JWT

--- src/main/java/io/yadnyesh/security/springbootjwtsecurity/controller/TokenController.java
+++ src/main/java/io/yadnyesh/security/springbootjwtsecurity/controller/TokenController.java
@@ -1,5 +1,18 @@
 package io.yadnyesh.security.springbootjwtsecurity.controller;
 
+import io.yadnyesh.security.springbootjwtsecurity.model.JwtUser;
+import io.yadnyesh.security.springbootjwtsecurity.security.JwtTokenGenerator;
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@RequestMapping("/token")
 public class TokenController {
 
+
+	@PostMapping("/{userName}")
+	public String generateJwtToken (@RequestBody final JwtUser jwtUser) {
+		
+		JwtTokenGenerator jwtTokenGenerator = new JwtTokenGenerator();
+		return jwtTokenGenerator.generate(jwtUser);
+	}
 }
